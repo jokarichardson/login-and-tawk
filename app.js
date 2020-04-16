17d2b55f947068fee4bdd1f13edd800323863f62
George Richardson <georgefdrichardson@gmail.com>
2020-04-16 10:39:11 -0300

    Adicionado outro canal jivochat

--- app.js
+++ app.js
@@ -10,38 +10,41 @@ app.use(express.static(path.join(__dirname, '/public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/about', function (req, res) {
+app.get('/about', function(req, res) {
     res.render('about');
 });
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     res.render('login/login');
 });
 
-app.post('/logged', function (req, res) {
+app.get('/jivo2', function(req, res) {
+    res.render('jivo2');
+});
+
+app.post('/logged', function(req, res) {
     console.log('Username inputed: ' + req.body.username);
     console.log('Password inputed: ' + req.body.password);
     console.log('Hidden field value: ' + req.body.hidDepartment);
     res.render('login/logged', { data: req.body });
 });
 
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
     res.status(404);
 
     res.format({
-        html: function () {
+        html: function() {
             res.render('error/404', { url: req.url })
         },
-        json: function () {
+        json: function() {
             res.json({ error: 'Not found' })
         },
-        default: function () {
+        default: function() {
             res.type('txt').send('Not found')
         }
     })
 });
 
-app.listen(process.env.PORT || 8080, function () {
+app.listen(process.env.PORT || 8080, function() {
     console.log('App em execução. Disponível na porta ' + process.env.PORT);
 });
-
